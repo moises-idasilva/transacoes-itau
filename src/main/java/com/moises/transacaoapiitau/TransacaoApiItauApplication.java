7f2c53c4a3d4933a2dd0113dd2196f5854e23946
Moises I da Silva <dev.moises.dasilva@gmail.com>
2025-01-30 15:29:41 -0300

    First commit

---
+++ src/main/java/com/moises/transacaoapiitau/TransacaoApiItauApplication.java
@@ -0,0 +1,13 @@
+package com.moises.transacaoapiitau;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransacaoApiItauApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TransacaoApiItauApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/moises/transacaoapiitau/TransacaoApiItauApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moises.transacaoapiitau;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransacaoApiItauApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
